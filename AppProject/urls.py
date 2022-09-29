ddcb7285812ac7430b3cddc9217b250bd9bf5f75
ABartoletti <71200173+ABartoletti@users.noreply.github.com>
2022-09-29 00:54:45 -0300

    Se realizan correcciones para la correcta ejecucion

--- AppProject/urls.py
+++ AppProject/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from AppProject.views import *
-#El debug esta ACA. Por algun motivo el contrenido de URLPATTERNS crashea. Si se borran los path levanta
+
 urlpatterns = [
     path('', home),
     path('create_driver/', create_driver),
@@ -9,7 +9,8 @@ urlpatterns = [
     path('read_user/', read_user),
     path('update_user/', update_user),
     path('delete_user/', delete_user ),
-    #path('create_driver/', create_driver),
-    #path('create_movile/', create_movile),
+    path('create_driver/', create_driver),
+    path('create_movile/', create_movile),
 ]
+
 
--- AppProject/views.py
+++ AppProject/views.py
@@ -6,12 +6,11 @@ from django.template import loader
 from pathlib import Path
 from AppProject.models import *
 from AppProject.forms import *
-path=Path(__file__).parent.resolve()
 # Create your views here.
 
 def create_user(request):
     if request.method == 'POST':
-        user = User(name= request.POST["nombre"], lastName= request.POST["apellido"], email= request.POST["email"])
+        user = User(name= request.POST["name"], lastName= request.POST["lastName"], email= request.POST["email"])
         user.save()
         users = User.objects.all() #trae todo
         return render (request, "CRUD/read_user.html", {"user":users})
@@ -47,14 +46,14 @@ def delete_user(request, user_email):
 
 def create_driver(request):
     if request.method == 'POST':
-        user = Driver(name= request.POST["nombre"], lastName= request.POST["apellido"], email= request.POST["email"], id=request.POST["ID"])
-        user.save()
+        driver = Driver(name= request.POST["name"], lastName= request.POST["lastName"], email= request.POST["email"], registry= request.POST["registry"])
+        driver.save()
     return render(request, "CRUD/create_driver.html")
 
 def create_movile(request):
     if request.method == 'POST':
-        user = Movile(carPatent= request.POST["patente"], carBrand= request.POST["marca"], year=request.POST["año"])
-        user.save()
+        movile = Movile(carPatent= request.POST["carPatent"], carBrand= request.POST["carBrand"], year=request.POST["carYear"])
+        movile.save()
     return render(request, "CRUD/create_movile.html")
 
 def home(request):
