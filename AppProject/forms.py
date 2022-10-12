3e61be5458d0aa234543a1b8269ff8430a319c43
Alejandro Giorgio <alegior7@gmail.com>
2022-10-11 23:54:57 -0300

    algo

--- AppProject/forms.py
+++ AppProject/forms.py
@@ -4,14 +4,12 @@ class form_user(forms.Form):
     name = forms.CharField(max_length=30)
     lastName = forms.CharField(max_length=30)
     email = forms.EmailField()
-
-#class form_driver(forms.Form):
-#    name = forms.CharField(max_length=30)
-#    lastName = forms.CharField(max_length=30)
-#    email = forms.EmailField()
-#    registry = forms.IntegerField()
-
-#class form_movile(forms.Form):
-#    carPatent = forms.CharField(max_length=30)
-#    carBrand = forms.CharField(max_length=30)
-#    year = forms.IntegerField()
+class form_driver(forms.Form):
+    name = forms.CharField(max_length=30)
+    lastName = forms.CharField(max_length=30)
+    email = forms.EmailField()
+    registry = forms.IntegerField()
+class form_movile(forms.Form):
+    carPatent = forms.CharField(max_length=30)
+    carBrand = forms.CharField(max_length=30)
+    year = forms.IntegerField()
